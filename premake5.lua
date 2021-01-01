d444d7bb7275f54a78aa84e32e9eae55fe2dc018
Ruben Falkenburg <rubenfalkenburg@gmail.com>
2021-01-01 23:57:28 +0100

    Update premake5 file

--- premake5.lua
+++ premake5.lua
@@ -25,20 +25,20 @@ project "glfw"
         
         files
         {
-            "win32_platform.h",
-            "win32_joystick.h",
-            "wgl_context.h",
-            "egl_context.h",
-            "osmesa_context.h",
-            "win32_init.c",
-            "win32_joystick.c",
-            "win32_monitor.c",
-            "win32_time.c",
-            "win32_thread.c",
-            "win32_window.c",
-            "wgl_context.c",
-            "egl_context.c",
-            "osmesa_context.c",
+            "src/win32_platform.h",
+            "src/win32_joystick.h",
+            "src/wgl_context.h",
+            "src/egl_context.h",
+            "src/osmesa_context.h",
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
         }
 
         defines "_GLFW_WIN32"
@@ -47,21 +47,21 @@ project "glfw"
         
         files
         {
-            "cocoa_platform.h",
-            "cocoa_joystick.h",
-            "posix_thread.h",
-            "nsgl_context.h",
-            "egl_context.h",
-            "osmesa_context.h",
-            "cocoa_init.m",
-            "cocoa_joystick.m",
-            "cocoa_monitor.m",
-            "cocoa_window.m",
-            "cocoa_time.c",
-            "posix_thread.c",
-            "nsgl_context.m",
-            "egl_context.c",
-            "osmesa_context.c",
+            "src/cocoa_platform.h",
+            "src/cocoa_joystick.h",
+            "src/posix_thread.h",
+            "src/nsgl_context.h",
+            "src/egl_context.h",
+            "src/osmesa_context.h",
+            "src/cocoa_init.m",
+            "src/cocoa_joystick.m",
+            "src/cocoa_monitor.m",
+            "src/cocoa_window.m",
+            "src/cocoa_time.c",
+            "src/posix_thread.c",
+            "src/nsgl_context.m",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
         }
 
         defines "_GLFW_COCOA"
